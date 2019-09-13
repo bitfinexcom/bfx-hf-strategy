8c8373505c025ba92c856b4e945dd7e1fe12ea1f
Cris Mihalache <me@f3rno.com>
2019-09-14 00:44:45 +0700

    (fix) examples [import path, candle vol key]

--- examples/exec_ema_cross.js
+++ examples/exec_ema_cross.js
@@ -2,7 +2,7 @@
 
 process.env.DEBUG = '*'
 
-const { Candle } = require('bitfinex-api-node/lib/models')
+const { Candle } = require('bfx-api-node-models')
 const { SYMBOLS, TIME_FRAMES } = require('bfx-hf-util')
 const logTrades = require('../lib/debug/log_trades')
 const HFS = require('../')
--- examples/render_iv_data.js
+++ examples/render_iv_data.js
@@ -1,17 +1,22 @@
 'use strict'
 
+process.env.DEBUG = '*'
+
 const FS = require('fs')
+const debug = require('debug')('bfx:hf:strategy:examples:render-iv-data')
 const { Candle } = require('bfx-api-node-models')
 const HFI = require('bfx-hf-indicators')
 const rawCandleData = require('./btc_candle_data.json')
 
+const OUTPUT_PATH = `${__dirname}/candle_iv_data.json`
 const candles = rawCandleData
   .sort((a, b) => a[0] - b[0])
   .map(c => new Candle(c).toJS())
-  .map(c => ({ ...c, vol: c.volume }))
 
 const iv = []
 const indicators = {}
+
+debug('loaded %d candles', candles.length)
 
 Object.keys(HFI).forEach(iName => {
   if (iName === 'Indicator') return
@@ -21,6 +26,8 @@ Object.keys(HFI).forEach(iName => {
   const key = C.id
 
   indicators[key] = new C(args)
+
+  debug('created %s instance', iName)
 })
 
 candles.forEach(candle => {
@@ -44,4 +51,10 @@ candles.forEach(candle => {
   iv.push(ivEntry)
 })
 
-FS.writeFileSync(`${__dirname}/candle_iv_data.json`, JSON.stringify(iv))
+debug('fed candles to all indicators')
+
+FS.writeFileSync(OUTPUT_PATH, JSON.stringify(iv))
+
+debug('wrote data to %s', OUTPUT_PATH)
+debug('sample first data point follows')
+debug(JSON.stringify(iv[0], null, 2))
