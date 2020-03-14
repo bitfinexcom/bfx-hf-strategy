55264fb443d0ae0c81e1b0b6991bdae7687000d7
Cris Mihalache <me@f3rno.com>
2020-03-14 17:34:24 +0700

    (fix) misc errors due to refactor, now working

---
+++ examples/ema_cross.js
@@ -0,0 +1,61 @@
+'use strict'
+
+process.env.DEBUG = '*'
+
+const debug = require('debug')('bfx:hf:strategy:examples:ema-cross')
+const { EMA } = require('bfx-hf-indicators')
+const { SYMBOLS, TIME_FRAMES } = require('bfx-hf-util')
+const { Candle } = require('bfx-api-node-models')
+const { execOffline } = require('bfx-hf-backtest')
+const HFSData = require('bfx-hf-strategy-data')
+const HFS = require('../')
+
+const SYMBOL = SYMBOLS.LEO_USD
+const TIME_FRAME = TIME_FRAMES.ONE_MINUTE
+
+const strategy = HFS.define({
+  id: 'ema_cross',
+  name: 'ema_cross',
+  symbol: SYMBOL,
+  tf: TIME_FRAME,
+
+  indicators: {
+    l: new EMA([100]),
+    s: new EMA([20])
+  },
+
+  onPriceUpdate: async function () {
+    const { s, l } = this.indicatorValues()
+
+    await this.condition.indicatorsCrossed('s', 'l')
+
+    if (!this.inAPosition()) {
+      return this.openPositionMarket({ amount: s > l ? 6 : -6 })
+    } else {
+      return this.closePositionMarket()
+    }
+  }
+})
+
+const run = async () => {
+  const candleKey = HFS.candleMarketDataKey({
+    symbol: SYMBOL,
+    tf: TIME_FRAME
+  })
+
+  return execOffline(strategy, {
+    candles: {
+      [candleKey]: HFSData['trade:1m:tLEOUSD'].map(arr => ({
+        ...new Candle(arr),
+        symbol: SYMBOL,
+        tf: TIME_FRAME
+      }))
+    }
+  })
+}
+
+try {
+  run()
+} catch (e) {
+  debug('error: %s', e.message)
+}
---
+++ lib/Session.vim
@@ -0,0 +1,50 @@
+let SessionLoad = 1
+let s:so_save = &so | let s:siso_save = &siso | set so=0 siso=0
+let v:this_session=expand("<sfile>:p")
+silent only
+cd ~/code/bfx
+if expand('%') == '' && !&modified && line('$') <= 1 && getline(1) == ''
+  let s:wipebuf = bufnr('%')
+endif
+set shortmess=aoO
+badd +53 bfx-hf-strategy/lib/init_helpers_for_strategy.js
+argglobal
+%argdel
+edit bfx-hf-strategy/lib/init_helpers_for_strategy.js
+set splitbelow splitright
+set nosplitright
+wincmd t
+set winminheight=0
+set winheight=1
+set winminwidth=0
+set winwidth=1
+argglobal
+setlocal fdm=marker
+setlocal fde=0
+setlocal fmr={{{,}}}
+setlocal fdi=#
+setlocal fdl=0
+setlocal fml=1
+setlocal fdn=20
+setlocal fen
+let s:l = 53 - ((52 * winheight(0) + 34) / 68)
+if s:l < 1 | let s:l = 1 | endif
+exe s:l
+normal! zt
+53
+normal! 04|
+lcd ~/code/bfx/bfx-hf-strategy/lib
+tabnext 1
+if exists('s:wipebuf') && getbufvar(s:wipebuf, '&buftype') isnot# 'terminal'
+  silent exe 'bwipe ' . s:wipebuf
+endif
+unlet! s:wipebuf
+set winheight=1 winwidth=20 winminheight=1 winminwidth=1 shortmess=filnxtToOFI
+let s:sx = expand("<sfile>:p:r")."x.vim"
+if file_readable(s:sx)
+  exe "source " . fnameescape(s:sx)
+endif
+let &so = s:so_save | let &siso = s:siso_save
+doautoall SessionLoadPost
+unlet SessionLoad
+" vim: set ft=vim :
