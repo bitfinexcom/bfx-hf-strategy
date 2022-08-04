eca5d30889545da4835e43c60715da6e8dd7a122
Tarcísio Zotelli Ferraz <tar-2008.ferraz@hotmail.com>
2022-08-04 09:10:41 -0300

    fix: calc gross pnl only (#38)
    * fix: calc gross pnl
    
    * update version
    
    * calc losses/gains based on positions
    
    Co-authored-by: Tarcisio Ferraz <tarcisio.ferraz@bitfinex.com>

--- test/unit/pnl/calc_realized_trade_pnl.js
+++ test/unit/pnl/calc_realized_trade_pnl.js
@@ -2,35 +2,31 @@
 
 const { expect } = require('chai')
 const { calcRealizedTradePnl } = require('../../../lib/pnl')
+const BigNumber = require('bignumber.js')
 
 describe('calc trade pnl', () => {
   const position = {
     inventory: []
   }
 
-  it('buy', () => {
-    const trade = {
-      price: 100,
-      amount: 1,
-      fee: 0.002
+  const fee = 0.002
+  const trades = [
+    { price: 31076.83, amount: 1, fee },
+    { price: 31599.44, amount: -0.16, fee },
+    { price: 31599.44, amount: 0.25, fee },
+    { price: 31770.26, amount: 0.25, fee },
+    { price: 31760.00, amount: -1.34, fee }
+  ]
+
+  it('calc gross pnl', () => {
+    let totalPnl = new BigNumber(0)
+
+    for (const trade of trades) {
+      const tradePnl = calcRealizedTradePnl(position, trade)
+      totalPnl = totalPnl.plus(tradePnl)
     }
 
-    const pnl = calcRealizedTradePnl(position, trade)
-
-    expect(pnl.toNumber()).to.eq(-0.2)
-    expect(position.inventory).to.have.length(1)
-  })
-
-  it('sell', () => {
-    const trade = {
-      price: 120,
-      amount: -1,
-      fee: 0.002
-    }
-
-    const pnl = calcRealizedTradePnl(position, trade)
-
-    expect(pnl.toNumber()).to.eq(19.96)
+    expect(totalPnl.toNumber()).to.eq(695.0554)
     expect(position.inventory).to.have.length(0)
   })
 })
