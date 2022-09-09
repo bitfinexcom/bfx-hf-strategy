43c8b6956fd0a31e492e7b08ba54457abbbac214
Tarcísio Zotelli Ferraz <tar-2008.ferraz@hotmail.com>
2022-09-09 08:21:25 -0300

    Trade fees (#39)

---
+++ test/unit/orders/calculate_fees.js
@@ -0,0 +1,82 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-env mocha */
+
+const { expect } = require('chai')
+const { stub, assert } = require('sinon')
+const calculateFees = require('../../../lib/orders/calculate_fees')
+
+describe('calc fees of trade', () => {
+  const stubAccountTrades = stub()
+  const rest = {
+    accountTrades: stubAccountTrades
+  }
+
+  afterEach(() => {
+    stubAccountTrades.reset()
+  })
+
+  const base = 'BTC'
+  const quote = 'USD'
+  const symbol = base + quote
+  const order = {
+    id: 103164770121
+  }
+  const orderParams = {
+    symbol,
+    submittedAt: 1661799613000
+  }
+
+  it('buy', async () => {
+    const trade = {
+      id: 1197423249,
+      symbol,
+      mtsCreate: 1661799613496,
+      orderID: 103164770121,
+      execAmount: 2,
+      execPrice: 20190,
+      orderType: 'EXCHANGE MARKET',
+      orderPrice: 20183,
+      maker: false,
+      fee: -0.004,
+      feeCurrency: base
+    }
+    stubAccountTrades.resolves([trade])
+
+    const fees = await calculateFees(rest, order, orderParams)
+
+    assert.calledWithExactly(stubAccountTrades, symbol, orderParams.submittedAt)
+    expect(fees).not.to.be.null
+    expect(fees.amount.toString()).to.be.eq('-0.004')
+    expect(fees.cost.toString()).to.be.eq('-80.76')
+    expect(fees.currency).to.be.eq(base)
+    expect(fees.perc.toString()).to.be.eq('0.002')
+    expect(fees.isMaker).to.be.eq(false)
+  })
+
+  it('sell', async () => {
+    const trade = {
+      id: 1197423249,
+      symbol,
+      mtsCreate: 1661799613496,
+      orderID: 103164770121,
+      execAmount: -1.996,
+      execPrice: 20190,
+      orderType: 'EXCHANGE MARKET',
+      orderPrice: 20183,
+      maker: false,
+      fee: -80.59848,
+      feeCurrency: quote
+    }
+    stubAccountTrades.resolves([trade])
+
+    const fees = await calculateFees(rest, order, orderParams)
+
+    assert.calledWithExactly(stubAccountTrades, symbol, orderParams.submittedAt)
+    expect(fees).not.to.be.null
+    expect(fees.amount.toString()).to.be.eq('-80.59848')
+    expect(fees.cost.toString()).to.be.eq('-80.59848')
+    expect(fees.currency).to.be.eq(quote)
+    expect(fees.perc.toString()).to.be.eq('0.002')
+    expect(fees.isMaker).to.be.eq(false)
+  })
+})
--- test/unit/pnl/calc_realized_trade_pnl.js
+++ test/unit/pnl/calc_realized_trade_pnl.js
@@ -9,13 +9,47 @@ describe('calc trade pnl', () => {
     inventory: []
   }
 
-  const fee = 0.002
   const trades = [
-    { price: 31076.83, amount: 1, fee },
-    { price: 31599.44, amount: -0.16, fee },
-    { price: 31599.44, amount: 0.25, fee },
-    { price: 31770.26, amount: 0.25, fee },
-    { price: 31760.00, amount: -1.34, fee }
+    {
+      price: 31076.83,
+      amount: 1,
+      fees: {
+        cost: new BigNumber(-62.15366),
+        amount: new BigNumber(-0.002)
+      }
+    },
+    {
+      price: 31599.44,
+      amount: -0.16,
+      fees: {
+        cost: new BigNumber(-10.1118208),
+        amount: new BigNumber(-0.16)
+      }
+    },
+    {
+      price: 31599.44,
+      amount: 0.25,
+      fees: {
+        cost: new BigNumber(-15.79972),
+        amount: new BigNumber(-0.0005)
+      }
+    },
+    {
+      price: 31770.26,
+      amount: 0.25,
+      fees: {
+        cost: new BigNumber(-15.88513),
+        amount: new BigNumber(-0.0005)
+      }
+    },
+    {
+      price: 31760.00,
+      amount: -1.34,
+      fees: {
+        cost: new BigNumber(-85.1168),
+        amount: new BigNumber(-1.34)
+      }
+    }
   ]
 
   it('calc gross pnl', () => {
@@ -26,7 +60,7 @@ describe('calc trade pnl', () => {
       totalPnl = totalPnl.plus(tradePnl)
     }
 
-    expect(totalPnl.toNumber()).to.eq(695.0554)
+    expect(totalPnl.toString()).to.eq('504.5467792')
     expect(position.inventory).to.have.length(0)
   })
 })
